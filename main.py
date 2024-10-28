b88a605a214a190247e3fd6699eaf58a597fb8d3
Katelyn Curtiss <153852200+KatelynCurtiss@users.noreply.github.com>
2024-10-28 10:50:11 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+# Katelyn Curtiss 
+# October 28 2024
+# List Basic Stats
+
+chem_quiz = [90, 89, 85, 96, 92, 86]
+
+num_elements = len(chem_quiz)
+average_chemscores = sum(chem_quiz) / num_elements
+
+print(f'Number of scores in your list: {num_elements}')
+print(f'Your average scores is: {average_chemscores}')
+
+
+# Driving Distances
+
+driving_distances = [2732.8, 747.2, 120.1, 87, 234.2] 
+
+average2 = sum(driving_distances) / len(driving_distances)
+
+min = min(driving_distances)
+max = max(driving_distances)
+
+print(f'Your lowest driving distance: {min}')
+print(f'Your highest driving distnace: {max}')
+
+print(f'You average distance {average2}')
+
+
